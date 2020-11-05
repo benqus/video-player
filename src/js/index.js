e79ff1d449684bb56b39ac035e6ad46bf9be4b2e
Bence Kormos <ben.kormos@gmail.com>
2020-11-05 19:04:29 +0000

    added play/pause and seek functionalities

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,52 @@
-
-console.log("HAI");
-
 const $video = document.getElementById('video');
+const $seekbar = document.getElementById('seekbar');
 const $progress = document.getElementById('progress');
-const $playButton = document.getElementById('play-button');
-const $pauseButton = document.getElementById('pause-button');
+const $buttons = document.getElementById('buttons');
 
 let interval;
 
-$playButton.onclick = (e) => {
-  $video.play();
-  if (!interval) {
-    interval = setInterval(updateProgressBar, 1000 / 30);
+// TODO 1. rewind/forward +/- [1,2,3,4,5] seconds
+// TODO 2. keyboard events left/right/space
+// TODO 3. add 2nd video player
+
+function toggleVideo() {
+  if ($video.paused) {
+    $video.play();
+    interval = interval ?? setInterval(updateProgressBar, 1000 / 30);
+  } else {
+    $video.pause();
+    clearInterval(interval);
+    interval = null;
   }
-};
-
-$pauseButton.onclick = (e) => {
-  $video.pause();
-  clearInterval(interval);
-  interval = null;
-};
-
-$video.onloadedmetadata = (...args) => {
-  console.log(...args);
+  renderButtons();
 }
-
-// $video.ontimeupdate = (e) => updateProgressBar(e);
 
 function updateProgressBar() {
   const { currentTime, duration } = $video;
   const progress = (currentTime / duration) * 100;
   $progress.style.right = `${100 - progress}%`;
-};
+}
+
+function renderButtons() {
+  $buttons.innerHTML = `<button class="video-toggle">${$video.paused ? 'play' : 'pause'}</button>`;
+}
+
+renderButtons();
+
+$seekbar.onclick = (e) => {
+  const { width } = e.target.getBoundingClientRect();
+  const { clientX } = e;
+
+  const position = clientX / width; // 0 - 1
+  const time = $video.duration * position;
+
+  $video.currentTime = time;
+  updateProgressBar();
+}
+
+document.body.addEventListener('click', (e) => {
+  const videoToggle = e.target.classList.contains('video-toggle');
+  if (videoToggle) {
+    toggleVideo();
+  }
+});
