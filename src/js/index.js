367c91ad618024967d64cd6173859e795d9b3fb1
Bence Kormos <ben.kormos@gmail.com>
2020-11-09 20:13:58 +0000

    adding keyboard events, rwd/fwd seek

--- src/js/index.js
+++ src/js/index.js
@@ -4,10 +4,14 @@ const $progress = document.getElementById('progress');
 const $buttons = document.getElementById('buttons');
 
 let interval;
+let jump = 5;
+let volume = 0.75;
 
 // TODO 1. rewind/forward +/- [1,2,3,4,5] seconds
-// TODO 2. keyboard events left/right/space
-// TODO 3. add 2nd video player
+// TODO 2. volume control
+// TODO 3. keyboard events left/right/space
+
+// TODO 4. add 2nd video player
 
 function toggleVideo() {
   if ($video.paused) {
@@ -28,25 +32,82 @@ function updateProgressBar() {
 }
 
 function renderButtons() {
-  $buttons.innerHTML = `<button class="video-toggle">${$video.paused ? 'play' : 'pause'}</button>`;
+  $buttons.innerHTML = `
+    <button class="video-toggle">${$video.paused ? 'play' : 'pause'}</button>
+    <select class="rew-fwd-select">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5" selected>5</option>
+    </select>
+    <button class="rwd">R</button>
+    <button class="fwd">F</button>
+    <input type="range" class="volume" min="0" max="100" step="1" value="${volume * 100}">
+  `;
 }
 
-renderButtons();
+function seekWith(sec) {
+  $video.currentTime += sec;
+  updateProgressBar();
+}
+
+function seekBackward() {
+  seekWith(-jump);
+}
+
+function seekForward() {
+  seekWith(jump);
+}
+
+function setVolume(newVolume) {
+  volume = $video.volume = Math.max(Math.min(newVolume, 1), 0);
+  renderButtons()
+}
+
+function increaseVolume() {
+  setVolume($video.volume + 0.05);
+}
+
+function decreaseVolume() {
+  setVolume($video.volume - 0.05);
+}
 
 $seekbar.onclick = (e) => {
   const { width } = e.target.getBoundingClientRect();
-  const { clientX } = e;
-
-  const position = clientX / width; // 0 - 1
-  const time = $video.duration * position;
-
-  $video.currentTime = time;
+  $video.currentTime = $video.duration * (e.clientX / width);
   updateProgressBar();
 }
 
 document.body.addEventListener('click', (e) => {
-  const videoToggle = e.target.classList.contains('video-toggle');
-  if (videoToggle) {
-    toggleVideo();
+  const { classList } = e.target;
+  if (classList.contains('video-toggle')) toggleVideo();
+  if (classList.contains('rwd')) seekBackward();
+  if (classList.contains('fwd')) seekForward();
+});
+
+document.body.addEventListener('change', (e) => {
+  const isSelect = e.target.classList.contains('rew-fwd-select');
+  if (isSelect) jump = parseInt(e.target.value);
+});
+
+document.body.addEventListener('input', (e) => {
+  const isVolume = e.target.classList.contains('volume');
+  
+  if (isVolume) {
+    volume = parseInt(e.target.value) / 100;
+    $video.volume = volume;
   }
 });
+
+document.body.addEventListener('keydown', (e) => {
+  switch(e.code) {
+    case 'ArrowLeft': seekBackward(); break;
+    case 'ArrowRight': seekForward(); break;
+    case 'ArrowUp': increaseVolume(); break;
+    case 'ArrowDown': decreaseVolume(); break;
+    case 'Space': toggleVideo(); break;
+  }
+});
+
+renderButtons();
